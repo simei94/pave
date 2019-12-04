805de5c7aad92e8840ae3d510d505f56cb41d520
tschlenther <schlenther@vsp.tu-berlin.de>
2019-12-04 17:47:48 +0100

    qsim module to bind the new reservation stuff

---
+++ src/main/java/org/matsim/drt/ReservingRequestsDvrpModeQSimModule.java
@@ -0,0 +1,65 @@
+package org.matsim.drt;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import org.matsim.contrib.drt.optimizer.VehicleData;
+import org.matsim.contrib.drt.optimizer.insertion.DefaultUnplannedRequestInserter;
+import org.matsim.contrib.drt.optimizer.insertion.InsertionCostCalculator;
+import org.matsim.contrib.drt.optimizer.insertion.PrecalculablePathDataProvider;
+import org.matsim.contrib.drt.optimizer.insertion.UnplannedRequestInserter;
+import org.matsim.contrib.drt.passenger.DrtRequest;
+import org.matsim.contrib.drt.passenger.DrtRequestCreator;
+import org.matsim.contrib.drt.run.DrtConfigGroup;
+import org.matsim.contrib.drt.scheduler.DrtScheduleInquiry;
+import org.matsim.contrib.drt.scheduler.RequestInsertionScheduler;
+import org.matsim.contrib.dvrp.fleet.Fleet;
+import org.matsim.contrib.dvrp.passenger.PassengerRequestCreator;
+import org.matsim.contrib.dvrp.run.AbstractDvrpModeQSimModule;
+import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.mobsim.framework.MobsimTimer;
+import org.matsim.drt.ReservationDecision;
+import org.matsim.drt.ReservedVehiclePoolRequestInserter;
+import org.matsim.drt.ReservingRequestCreator;
+
+public class ReservingRequestsDvrpModeQSimModule extends AbstractDvrpModeQSimModule {
+    DrtConfigGroup drtCfg;
+
+    public ReservingRequestsDvrpModeQSimModule(DrtConfigGroup drtCfg) {
+        super(drtCfg.getMode());
+        this.drtCfg = drtCfg;
+    }
+
+    @Override
+    protected void configureQSim() {
+        //override the request inserter
+
+        addModalComponent(ReservedVehiclePoolRequestInserter.class, modalProvider(
+                getter -> new ReservedVehiclePoolRequestInserter(drtCfg, getter.getModal(Fleet.class),
+                        getter.get(MobsimTimer.class), getter.get(EventsManager.class),
+                        getter.getModal(RequestInsertionScheduler.class),
+                        getter.getModal(VehicleData.EntryFactory.class),
+                        getter.getModal(PrecalculablePathDataProvider.class),
+                        getter.getModal(InsertionCostCalculator.PenaltyCalculator.class),
+                        getter.getModal(DrtScheduleInquiry.class))));
+        bindModal(UnplannedRequestInserter.class).to(modalKey(ReservedVehiclePoolRequestInserter.class));
+
+
+        bind(ReservationDecision.class).toInstance(agent -> agent.getId().toString().contains("freight"));
+
+        bindModal(PassengerRequestCreator.class).toProvider(new Provider<PassengerRequestCreator>() {
+            @Inject
+            private EventsManager events;
+            @Inject
+            private MobsimTimer timer;
+
+            @Inject
+            private ReservationDecision reservationDecision;
+
+            @Override
+            public PassengerRequestCreator get() {
+                return new ReservingRequestCreator(getMode(), events, timer, reservationDecision);
+            }
+        }).asEagerSingleton();
+
+    }
+}
