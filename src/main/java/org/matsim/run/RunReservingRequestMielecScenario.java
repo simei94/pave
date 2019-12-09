b8ebc15384ebe3f6d5e72daca2000df0be7be924
tschlenther <schlenther@vsp.tu-berlin.de>
2019-12-09 14:54:30 +0100

    rename run class

--- src/main/java/org/matsim/run/RunReservingRequestMielecScenario.java
+++ src/main/java/org/matsim/run/RunReservingRequestMielecScenario.java
@@ -32,7 +32,7 @@ import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 
-public class RunReservingRequestEquilScenario {
+public class RunReservingRequestMielecScenario {
 
     private static boolean otfvis = false;
 
